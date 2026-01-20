8dbd46429fbd129afc93828701d7cf7b3232b74a
danghanhuthien <151927921+danghanhuthien@users.noreply.github.com>
2026-01-20 10:41:39 +0700

    Update config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// API Configuration
+const CONFIG = {
+    API_URL: 'https://script.google.com/macros/s/AKfycbzIKlkryJvmaUHB5tFMvjwObJSww6ecqDBTDz_eFQAeVQUCmBk_PnbIRLUx8LpAh3qK/exec',
+    SPREADSHEET_ID: '1swxvw6EKKoskgqtypSSlhfWz_2_GeRBws2VEMmzJluY'
+};
+
+// Export for use in other files
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = CONFIG;
+}
